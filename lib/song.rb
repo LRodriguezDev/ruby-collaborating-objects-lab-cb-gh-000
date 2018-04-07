1021aeffcf110327a9b26fcc2900277668dc057d
Luis Rodriguez <zacha@tutanota.com>
2018-04-07 03:30:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,35 +1,21 @@
 class Song
   attr_accessor :name, :artist
 
-  @@all = []
-
   def initialize(name)
     @name = name
   end
 
+  def artist_name=(artist_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    @artist = artist
+    artist.save
+    @artist
+  end
+
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song.save
-  end
-
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
-    end
-  end
-
-  def self.all
-    @@all
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  end
-
-  def save
-    @@all << self
-    self
+    song_name = filename.split(" - ")[1]
+    song = self.new(song_name)
+    song.artist_name = filename.split(" - ")[0]
+    song
   end
 end
