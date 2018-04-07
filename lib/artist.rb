73a596df9f6b486a7b3bb7b0e1ece4f0e1421503
Luis Rodriguez <zacha@tutanota.com>
2018-04-07 03:29:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,16 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def songs
-    Song.find_by_artist(self)
+  def add_song(song)
+    @songs << song
+    song.artist = self
   end
 
   def self.all
@@ -30,7 +32,7 @@ class Artist
   end
 
   def print_songs
-    songs.each do |song|
+    @songs.each do |song|
       puts song.name
     end
   end
