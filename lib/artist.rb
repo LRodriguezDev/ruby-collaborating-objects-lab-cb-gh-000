90caf7445ae7181fa9ce7f04f60564fc6970cd59
Luis Rodriguez <zacha@tutanota.com>
2018-04-07 03:27:00 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(artist_name)
+    artist = @@all.detect {|artist| artist.name == artist_name }
+    if artist != nil
+      artist
+    else
+      artist = self.new(artist_name)
+      artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
